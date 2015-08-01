e36a78323ba3bfb764f173073883f9c9121499e3
楢木 悠士 <yuji.1277@gmail.com>
2015-08-01 09:17:20 +0900

    Initial Commit

---
+++ createBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  createBox
+//
+//  Created by 楢木 悠士 on 2015/08/01.
+//  Copyright (c) 2015年 yuji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
